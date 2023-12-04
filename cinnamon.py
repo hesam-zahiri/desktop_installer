59377b5ba2fcda09d7837b09ecef5ad12aa87155
hesam-zahiri <139108183+hesam-zahiri@users.noreply.github.com>
2023-12-04 09:03:38 +0330

    Update cinnamon.py

--- cinnamon.py
+++ cinnamon.py
@@ -33,7 +33,7 @@ if distribution == "a":
   subprocess.Popen(["sudo", "pacman", "-Sy", "cinnamon"]).wait()
   subprocess.Popen(["sudo", "systemctl", "enable", "cinnamon-session"]).wait()
   subprocess.Popen(["sudo", "systemctl", "start", "cinnamon-session"]).wait()
-  subprocess.Popen(["sudo", "rebbot"]).wait()
+  subprocess.Popen(["sudo", "reboot"]).wait()
 
 # If the distribution is Debian Base, run the codes for installing the cinamon desktop on Debian
 
@@ -41,7 +41,7 @@ elif distribution == "d":
   print("Your distribution is debian base.")
   subprocess.Popen(["sudo", "apt-get", "install", "cinnamon"]).wait()
   subprocess.Popen(["sudo", "update-alternatives", "--config", "x-session-manager"]).wait()
-  subprocess.Popen(["sudo", "rebbot"]).wait()
+  subprocess.Popen(["sudo", "reboot"]).wait()
 
 # If it is Red Hat Base distribution, run the codes related to installing cinamon desktop on Red Hat
 
@@ -50,7 +50,7 @@ elif distribution == "r":
   subprocess.Popen(["sudo", "yum", "install", "cinnamon"]).wait()
   subprocess.Popen(["sudo", "systemctl", "enable", "cinnamon-session"]).wait()
   subprocess.Popen(["sudo", "systemctl", "start", "cinnamon-session"]).wait()
-  subprocess.Popen(["sudo", "rebbot"]).wait()
+  subprocess.Popen(["sudo", "reboot"]).wait()
 
 # If it's another distribution, send an error message
 
